37a192bdac36509bcc692eb5304e1dfb453d5b8f
lukasz94w <lukasz94w@wp.pl>
2023-10-30 21:26:05 +0100

    Add detection of lost connection to server

--- src/WebSocketClient.tsx
+++ src/WebSocketClient.tsx
@@ -7,17 +7,55 @@ const WebSocketClient = () => {
     const [receivedMessage, setReceivedMessage] = useState('');
 
     useEffect(() => {
+        let heartbeatIntervalId: NodeJS.Timer
+        let heartbeatCheckingId: NodeJS.Timer;
+
         const sockJS = new SockJS('http://localhost:8092/websocket');
 
         sockJS.onopen = () => {
             console.log("Connected to WebSocket");
             setSocket(sockJS);
+
+            heartbeatIntervalId = setInterval(() => {
+                sockJS.send(JSON.stringify({
+                    message: "heartbeat_iss"
+                }));
+            }, 30000);
+
+            heartbeatCheckingId = setInterval(() => {
+                // show message lost connection to server and navigate to main window
+                sockJS.close();
+            }, 35000);
         };
 
         sockJS.onmessage = (event: any) => {
-            setReceivedMessage(event.data);
+            if (event.data === "heartbeat_ack") {
+                console.log("Received confirmation, clearing the timer...")
+                clearInterval(heartbeatCheckingId);
+            } else if (event.data === "paired_session_is_lost") {
+                // show message paired session lost the connection
+                sockJS.close(); // and then because onclose is called automatic navigation to main window is executed?
+            } else {
+                setReceivedMessage(event.data);
+            }
         };
 
+        sockJS.onerror = () => {
+            sockJS.close(); // I can image it can occur where for example paired session lost the connection
+        }
+
+        sockJS.onclose = () => {
+            console.log("Onclose is called, I am going back to the main window...")
+            clearInterval(heartbeatIntervalId);
+            clearInterval(heartbeatCheckingId);
+            // TODO: go back to main window
+        }
+
+        return () => {
+            if (sockJS) {
+                sockJS.close();
+            }
+        };
     }, []);
 
     const sendMessage = () => {
