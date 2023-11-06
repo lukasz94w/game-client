cda6ef74f6cd371b601629194d8745e7ba4c528a
lukasz94w <lukasz94w@wp.pl>
2023-11-06 18:23:10 +0100

    Introduce main page and navigation logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import WebSocketClient from './WebSocketClient';
 import reportWebVitals from './reportWebVitals';
+import App from "./App";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <WebSocketClient/>
+    <App></App>
 );
 
 // If you want to start measuring performance in your app, pass a function
