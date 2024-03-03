486d046b37208ca80bbec487de47a9f2d28d2fca
lukasz94w <lukasz94w@wp.pl>
2024-03-03 16:55:28 +0100

    Add headerType to json message send during websocket connection to ease determining type of message

---
+++ src/api/json/common/JsonKey.ts
@@ -0,0 +1,2 @@
+export const MESSAGE_TYPE = "messageType";
+export const DATA = "data";
---
+++ src/api/json/outgoing/ClientMessageTypeValue.ts
@@ -0,0 +1,7 @@
+export const PLAYER_MESSAGE = "playerMessage";
+
+export const GAME_UPDATE = "gameUpdate";
+
+export const HEARTBEAT = "heartbeat";
+
+export const PLAYER_RECEIVED_GAME_UPDATE_CONFIRMATION = "playerReceivedGameUpdateConfirmation";
