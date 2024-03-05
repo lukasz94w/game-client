675cb8fdf6fce1c8d05c26bc9aab906fe07b0439
lukasz94w <lukasz94w@wp.pl>
2024-03-05 20:05:45 +0100

    Introduce improvements in checking whether user logged in and add some other minor changes

--- src/navbar/AuthNavbar.tsx
+++ src/navbar/AuthNavbar.tsx
@@ -37,7 +37,7 @@ const AuthNavbar = () => {
     // Here is request send to one of such endpoints.
     const refreshSessionPeriodically = () => {
         return setInterval(() => {
-            authService.verifySignedIn();
+            authService.refreshSession();
         }, THIRTY_MINUTES_REFRESH_SESSION_INTERVAL)
     }
 
